e6cf06a78916d7598db468606230b1f30a667a20
beholdenkey <44618918+beholdenkey@users.noreply.github.com>
2021-06-16 16:51:57 -0400

    Update gitlab.rb

--- GitLab/gitlab.rb
+++ GitLab/gitlab.rb
@@ -1,3 +1,9 @@
+## gitlab.yml configuration
+#################################################################################################
+gitlab_rails['time_zone'] = 'America/New_York'
+#################################################################################################
+### OmniAuth Settings
+#################################################################################################
 gitlab_rails['omniauth_enabled'] = true
 gitlab_rails['omniauth_allow_single_sign_on'] = ['saml']
 gitlab_rails['omniauth_block_auto_created_users'] = false
@@ -5,14 +11,15 @@ gitlab_rails['omniauth_auto_link_saml_user'] = true
 gitlab_rails['omniauth_providers'] = [
   {
     name: 'saml',
-    label: 'SAML',
+    label: 'Keycloak Login',
     args: {
-      assertion_consumer_service_url: 'https://gitlab.example.com/users/auth/saml/callback',
-      idp_cert: "-----BEGIN CERTIFICATE-----\nMII...\n-----END CERTIFICATE-----\n",
-      idp_sso_target_url: 'https://keycloak.example.com/auth/realms/hello/protocol/saml',
-      issuer: 'https://gitlab.example.com',
+      assertion_consumer_service_url: 'http://192.168.99.201/users/auth/saml/callback',
+      idp_cert: '-----BEGIN CERTIFICATE-----\nMII...\n-----END CERTIFICATE-----\n',
+      idp_sso_target_url: 'http://192.168.128.11:8080/auth/realms/Blackwood/protocol/saml',
+      issuer: 'http://192.168.99.201',
       name_identifier_format: 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
       attribute_statements: { username: ['username'] }
+      allowed_clock_drift: 5
     }
   }
 ]
