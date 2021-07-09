fe2cc09ede9f4240394deac4280a4e3d6508587e
beholdenkey <44618918+beholdenkey@users.noreply.github.com>
2021-07-09 10:33:31 -0400

    lots of changes

--- Pulp/Installation/Ansible/server-setup.sh
+++ Pulp/Installation/Ansible/server-setup.sh
@@ -3,7 +3,43 @@
 sudo systemctl enable --now
 
 sudo dnf -y update && \
-  dnf -y install \
+  dnf -y install && \
   nano \
-  net-tools \
+  net-tools \  
 
+dnf -y install epel-release
+sudo dnf -y install ansible
+dnf groupinstall "Development Tools" -y
+dnf install openssl-devel libffi-devel bzip2-devel wget -y
+
+wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+
+tar xvf Python-3.9.6.tgz
+
+cd Python-3.9*/
+
+./configure --enable-optimizations
+
+sudo make altinstall
+
+PATH=”$PATH:/usr/local/bin/python”
+
+python3.9 --version
+# To upgrade to ansible-core:
+pip3.9 uninstall ansible-base
+pip3.9 install ansible-core
+# To upgrade to Ansible 4:
+pip3.9 uninstall ansible-base
+pip3.9 install ansible
+
+# 1 - Install the pulp_installer collection:
+
+ansible-galaxy collection install pulp.pulp_installer
+
+# 2 - Install the geerlingguy.postgresql role:
+
+ansible-galaxy install geerlingguy.postgresql
+
+# 3 - Write the following playbook:
+
+nano install.yml
