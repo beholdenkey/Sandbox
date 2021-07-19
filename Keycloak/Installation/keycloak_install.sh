4f5c97118987a724b434eb342097514c0e210b89
beholdenkey <44618918+beholdenkey@users.noreply.github.com>
2021-07-19 16:51:02 -0400

    Update keycloak_install.sh

--- Keycloak/Installation/keycloak_install.sh
+++ Keycloak/Installation/keycloak_install.sh
@@ -23,10 +23,42 @@ wget https://download.oracle.com/otn-pub/java/jdk/16.0.1+9/7147401fd7354114ac51e
 
 dnf -y localinstall jdk-16.0.1_linux-x64_bin.rpm
 
-cd /opt/java/
+cd /opt/
 
 wget https://github.com/keycloak/keycloak/releases/download/14.0.0/keycloak-14.0.0.tar.gz
 tar -xvzf keycloak-14.0.0.tar.gz
 mv keycloak-14.0.0.tar.gz keycloak
 
-cd
+ln -s /opt/keycloak/keycloak-14.0.0 /opt/keycloak/current
+
+/opt/keycloak/bin/add-user-keycloak.sh -r master -u admin -p admin
+
+/opt/keycloak/bin/standalone.sh -b=0.0.0.0
+
+cat > /etc/systemd/system/keycloak.service <<EOF
+[Unit]
+Description=Keycloak
+After=network.target
+
+[Service]
+Type=idle
+User=keycloak
+Group=keycloak
+ExecStart=/bin/bash /opt/keycloak/current/bin/standalone.sh
+TimeoutStartSec=600
+TimeoutStopSec=600
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+mkdir/var/log/keycloak
+useradd --system --user-group keycloak
+chown -R keycloak:keycloak/opt/keycloak
+chown -R keycloak:keycloak/var/log/keycloak
+chmod -R g+w/opt/keycloak
+chmod -R g+w/var/log/keycloak
+
+systemctl daemon-reload
+systemctl enable keycloak
+systemctl start keycloak
