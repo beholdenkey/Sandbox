123a13cc7ba3263749e2f4c333aa7c803cd74854
beholdenkey <44618918+beholdenkey@users.noreply.github.com>
2021-07-19 16:44:54 -0400

    Update keycloak_install.sh

--- Keycloak/Installation/keycloak_install.sh
+++ Keycloak/Installation/keycloak_install.sh
@@ -9,3 +9,24 @@ if [ "$CONT" = "y" ]; then
   else
     echo "Aborted"
   fi
+
+dnf -y update && \
+    dnf -y install \
+    wget
+
+firewall-cmd --zone=public --add-service=http --permanent
+firewall-cmd --zone=public --add-service=https --permanent
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
+firewall-cmd --reload
+
+wget https://download.oracle.com/otn-pub/java/jdk/16.0.1+9/7147401fd7354114ac51ef3e1328291f/jdk-16.0.1_linux-x64_bin.rpm
+
+dnf -y localinstall jdk-16.0.1_linux-x64_bin.rpm
+
+cd /opt/java/
+
+wget https://github.com/keycloak/keycloak/releases/download/14.0.0/keycloak-14.0.0.tar.gz
+tar -xvzf keycloak-14.0.0.tar.gz
+mv keycloak-14.0.0.tar.gz keycloak
+
+cd
