6b1165958d5866d21d4365184fcad0a6d1b2f660
beholdenkey <44618918+beholdenkey@users.noreply.github.com>
2021-07-19 17:06:31 -0400

    Update keycloak_install.sh

--- Keycloak/Installation/keycloak_install.sh
+++ Keycloak/Installation/keycloak_install.sh
@@ -21,15 +21,19 @@ wget https://download.oracle.com/otn-pub/java/jdk/16.0.1+9/7147401fd7354114ac51e
 
 dnf -y localinstall jdk-16.0.1_linux-x64_bin.rpm
 
-cd /opt/
+groupadd -r keycloak
+useradd -m -d /var/lib/keycloak -s /sbin/nologin -r -g 
+keycloak keycloak
+mkdir -p /opt/keycloak
 
-wget https://github.com/keycloak/keycloak/releases/download/14.0.0/keycloak-14.0.0.tar.gz
-tar -xvzf keycloak-14.0.0.tar.gz
-mv keycloak-14.0.0.tar.gz keycloak
+wget https://github.com/keycloak/keycloak/releases/download/14.0.0/keycloak-14.0.0.tar.gz -P /opt/keycloak
+tar -xvzf keycloak-14.0.0.tar.gz -d /opt/keycloak
 
 ln -s /opt/keycloak/keycloak-14.0.0 /opt/keycloak/current
 
-/opt/keycloak/bin/add-user-keycloak.sh -r master -u admin -p admin
+chown keycloak: -R /opt/keycloak
+
+chmod 700 /opt/keycloak/current/standalone
 
 /opt/keycloak/bin/standalone.sh -b=0.0.0.0
 
