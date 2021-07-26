0c3fd9a299e708eeb0eb5be002f8e10b4b446e5d
beholdenkey <44618918+beholdenkey@users.noreply.github.com>
2021-07-26 14:48:32 -0400

    lots of changes

--- Keycloak/Installation/keycloak_install.sh
+++ Keycloak/Installation/keycloak_install.sh
@@ -7,38 +7,34 @@ if [ "$CONT" = "y" ]; then
   else
     echo "Aborted"
   fi
-
+echo "Update OS and Installing Dependencies"
+echo "Please be Patient this may take sometime"
 dnf -y update && \
     dnf -y install \
     wget
+    java-16-openjdk-devel
 
+
+groupadd -r keycloak
+useradd -m -d /var/lib/keycloak -s /sbin/nologin -r -g keycloak keycloak
+
+echo "Configuring Firewall"
 firewall-cmd --zone=public --add-service=http --permanent
 firewall-cmd --zone=public --add-service=https --permanent
 firewall-cmd --zone=public --add-port=80/tcp --permanent
 firewall-cmd --zone=public --add-port=443/tcp --permanent
 firewall-cmd --reload
 
-wget https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_linux-x64_bin.tar.gz
-
-tar zxvf openjdk-16.0.2_linux-x64_bin.tar.gz
-dnf -y localinstall jdk-16.0.1_linux-x64_bin.rpm
-
-groupadd -r keycloak
-useradd -m -d /var/lib/keycloak -s /sbin/nologin -r -g 
-keycloak keycloak
-
 mkdir -p /opt/keycloak
 
 wget https://github.com/keycloak/keycloak/releases/download/14.0.0/keycloak-14.0.0.tar.gz -P /opt/keycloak
-tar -xvzf keycloak-14.0.0.tar.gz -d /opt/keycloak
+tar -xvzf keycloak-14.0.0.tar.gz
 
 ln -s /opt/keycloak/keycloak-14.0.0 /opt/keycloak/current
 
 chown keycloak: -R /opt/keycloak
 
 chmod 700 /opt/keycloak/current/standalone
-
-/opt/keycloak/bin/standalone.sh -b=0.0.0.0
 
 cat > /etc/systemd/system/keycloak.service <<EOF
 [Unit]
@@ -56,13 +52,6 @@ TimeoutStopSec=600
 [Install]
 WantedBy=multi-user.target
 EOF
-
-mkdir/var/log/keycloak
-useradd --system --user-group keycloak
-chown -R keycloak:keycloak/opt/keycloak
-chown -R keycloak:keycloak/var/log/keycloak
-chmod -R g+w/opt/keycloak
-chmod -R g+w/var/log/keycloak
 
 systemctl daemon-reload
 systemctl enable keycloak
---
+++ Keycloak/container/kc_container.sh
@@ -0,0 +1,1 @@
+podman run -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:14.0.0
