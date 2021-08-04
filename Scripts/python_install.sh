1b55a4473d829303dae784db0309150af266bf05
beholdenkey <44618918+beholdenkey@users.noreply.github.com>
2021-08-03 21:00:13 -0400

    added python installation script

---
+++ Scripts/python_install.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+#------------------------------------------------------------ #
+# By @beholdenkey
+# Compatible Operating systems: Centos 8 Stream, RHEL 8, Fedora 34
+# Script to install Python 3.9.6 on CentOS 8 Stream and RHEL 8
+#------------------------------------------------------------- #
+
+sudo dnf -y update && \
+    dnf -y install \
+    wget && \
+dnf groupinstall "Development Tools" -y
+
+wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+
+tar xvf Python-3.9.6.tgz
+
+cd Python-3.9*/
+
+./configure --enable-optimizations
+
+sudo make altinstall
+
+PATH=”$PATH:/usr/local/bin/python”
+
+rm -rf Python-3.9.6.tgz
+rm -rf Python-3.9.6
+ 
+python3.9 --version
